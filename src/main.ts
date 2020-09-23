41c33709cb41ca382d6f26b456978f90c575ab46
erislandio.soares <erislandio.soares@acct.global>
2020-09-22 22:40:58 -0300

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as rateLimit from 'express-rate-limit';
 
 (async () => {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
+  app.use(
+    rateLimit({
+      max: 100,
+    }),
+  );
+  app.enableCors();
   await app.listen(3000);
 })();
