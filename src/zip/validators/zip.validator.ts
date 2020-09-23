eab627b4ddb9f2df6bc68cb24f52ecef68afb39a
erislandio.soares <erislandio.soares@acct.global>
2020-09-22 23:52:01 -0300

    settings zip validation pipe

--- src/zip/validators/zip.validator.ts
+++ src/zip/validators/zip.validator.ts
@@ -3,11 +3,11 @@ import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 @Injectable()
 export class ZipValidatorPipe implements PipeTransform {
   transform(zip: string): string {
-    const valid = /[0-9]{5}-[\d]{3}/g;
+    const valid = /[0-9]{8}/g;
 
     const result = valid.test(zip);
 
-    if (!result) {
+    if (!result || zip.length > 8) {
       throw new BadRequestException('Please enter a valid zip code');
     }
 
